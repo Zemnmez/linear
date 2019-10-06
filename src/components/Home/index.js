f522bd7b220e95af6384088ba6aebd68bcd6804c
tshadwel <tshadwel@amazon.com>
2019-10-06 15:49:15 -0700

    committing this because its kind of snazzy before i delete it

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import style from './Home.module.css';
 import SadHumans from 'components/SadHumans';
 import Timeline from 'components/Timeline';
+import Link from 'components/Link'
+import { classes } from 'lib/classes'
 
 export const Home = ({data: bio, className }) => {
   const { handle, name } = bio.who;
+  const { links } = bio;
   return <div className={[ style.Home ].concat(className).join(" ")}>
     <SadHumans className={style.logo}/>
     <div className={style.handle}>{handle}</div>
     <div className={style.name}><Names {...{names: name}}/></div>
+
+    <Links {...{
+      links,
+    }}/>
 
     <Timeline {...{
       timeline: bio.timeline,
@@ -16,6 +23,13 @@ export const Home = ({data: bio, className }) => {
     }}/>
   </div>
 }
+
+export const Links = ({ links, className }) => <div {...{
+  className: classes(className, style.links)
+}}>
+  {Object.entries(links).map(([name, href]) =>
+    <Link {...{href}}>{name}</Link>)}
+</div>
 
 export const Names = ({names}) => names.map(
   n => n instanceof Array?
