f0d22a1a8ec8b30d126a0014cb44cc0feefe16c0
tshadwel <tshadwel@amazon.com>
2019-10-05 23:13:32 -0700

    fixed some things, broke some more things not sure why im not asleep

--- src/components/Home/Home.stories.js
+++ src/components/Home/Home.stories.js
@@ -8,7 +8,7 @@ import { App } from 'components/App/App.module.css';
 import { base } from 'components/colors.module.css';
 
 import bio from 'bio'
-import Home from 'components/Home';
+import { Home } from 'components/Home';
 
 let stories = storiesOf(module)
     .add('default', () => <Home {...{
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import style from './Home.module.css';
+import SadHumans from 'components/SadHumans';
+import Timeline from 'components/Timeline';
 
 
-export const Home = ({data, className}) => {
-  data.who.name=undefined; // just dont want it lol
-
+export const Home = ({data: bio, className }) => {
   return <div className={[ style.home ].concat(className).join(" ")}>
     <Header {...bio}/>
+
+    <Timeline {...{timeline: bio.timeline}}/>
   </div>
 }
 
-const Header = ({ who: {names, handle}}) =>
+const Header = ({ who: {handle, name}}) =>
   <header className={style.Header}>
     <SadHumans className={style.logo} />
     <div className={style.handle}>{handle}</div>
-    <div className={style.name}><Names {...{names}}/></div>
+    <div className={style.name}><Names {...{names: name}}/></div>
 </header>
 
 const Names = ({names}) => names.map(
   n => n instanceof Array?
     n.map(([firstLetter]) => firstLetter).join(""):
-    nn).join(" ");
+    n).join(" ");
